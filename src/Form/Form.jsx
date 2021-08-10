0b862668a2a70eed3e1de236bdb178866b4825b8
chery-pynya <nikita.cherepnya@gmail.com>
2021-08-10 21:07:11 +0300

    done

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -4,47 +4,40 @@ import Error from './Error/Error';
 
 import { PropTypes } from 'prop-types';
 
-export default function Form({backgroundChange, hex, isHex}){
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const reg = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.test(str);
-        if (reg) {
-            backgroundChange({
-                hex: str,
-                isHex: reg,
-            });
-        } else {
-            backgroundChange({
-                hex: '#fff',
-                isHex: reg,
-            });
-        }
-    };
-    let str;
-    const handleInput = (e) => {
-        str = e.target.value;
-    };
-    return (
-        <form className='Form' onSubmit={handleSubmit}>
-            <input 
-            className='Form-input' 
-            name='hex'
-            onChange={handleInput}
-            />
-            {isHex === true ? <RGB hex={hex} /> 
-            : isHex === false ? <Error />
-            : false
-            }
-        </form>
-    );
+export default function Form({ backgroundChange, hex, isHex }) {
+  const reg = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i
+  const handleInput = (e) => {
+    if (e.target.value.length === 7) {
+      const check = reg.test(
+        e.target.value
+      );
+      if (check) {
+        backgroundChange({
+          hex: e.target.value,
+          isHex: check,
+        });
+        return;
+      }
+      backgroundChange({
+        hex: '#fff',
+        isHex: check,
+      });
+    }
+  };
+  return (
+    <form className='Form'>
+      <input className='Form-input' name='hex' onChange={handleInput} />
+      {isHex === true ? <RGB hex={hex} /> : isHex === false ? <Error /> : false}
+    </form>
+  );
 }
 
 Form.propTypes = {
-    backgroundChange: PropTypes.func.isRequired,
-    hex: PropTypes.string.isRequired,
-    isHex: PropTypes.bool,
-}
+  backgroundChange: PropTypes.func.isRequired,
+  hex: PropTypes.string.isRequired,
+  isHex: PropTypes.bool,
+};
 
 Form.defaultProps = {
-    isHex: undefined,
-}
+  isHex: undefined,
+};
